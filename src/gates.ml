e1d575f35ea07105e37d535c1043cbba96ccf8ba
Spiros Eliopoulos <seliopou@gmail.com>
2014-10-27 15:11:00 -0400

    main: enable discovery and whitelist some MACs

--- src/gates.ml
+++ src/gates.ml
@@ -1,9 +1,17 @@
 open Core.Std
 open Async.Std
 
+module C = Async_NetKAT_Controller
+
+
 let main () : unit =
   let open Deferred in
   let ctl, app = Gates_app.create () in
-  don't_wait_for (ignore (Async_NetKAT_Controller.start app ()))
+  let thread = C.start app ()
+    >>= fun t ->
+      C.enable_discovery t >>= fun () ->
+      List.iter ~f:(Gates_app.register_mac ctl) [1L; 2L; 3L]
+  in
+  don't_wait_for thread
 
 let () = never_returns (Scheduler.go_main ~main ())
